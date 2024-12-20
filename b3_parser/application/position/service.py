ce3bca28c10c723bd83d56535c887a937dedf2e5
Guilherme <guilherme.ramos@outlook.com>
2024-12-20 12:37:09 -0300

    adicionado peso ideal em constants

--- b3_parser/application/position/service.py
+++ b3_parser/application/position/service.py
@@ -1,9 +1,40 @@
+from b3_parser.core.models.position_model import PositionModel
 from b3_parser.core.repositories.position_repository import PositionRepository
 from b3_parser.core.repositories.transaction_repository import TransactionRepository
 from b3_parser.utils.xlsx.xlsx_parser import XLSXParser
+
+from typing import List
+
+
+class PositionService:
+
+    def __init__(self, position_repository: PositionRepository):
+        self._position_repository = position_repository
+
+    @staticmethod
+    def _format_positions(positions: List[PositionModel]):
+        formatted_positions = []
+        for pos in positions:
+            formatted_positions.append({
+                'product': pos.product_id,
+                'pm': pos.pm,
+                'qtd': pos.qtd,
+                'type': pos.type,
+                'ideal': pos.ideal
+            })
+        return formatted_positions
+
+    def get_positions(self):
+        position_models = self._position_repository.get_all_positions()
+        return self._format_positions(position_models)
+
 
 if __name__ == '__main__':
     xlsx_parser = XLSXParser()
     transaction_repository = TransactionRepository(xlsx_parser)
     position_repository = PositionRepository(transaction_repository)
-    position_repository.get_all_positions()
+
+    service = PositionService(position_repository)
+    ret = service.get_positions()
+
+    print(ret)
--- b3_parser/core/models/position_model.py
+++ b3_parser/core/models/position_model.py
@@ -8,81 +8,120 @@ class PositionModel:
 
     def __init__(self, product_id: str, transactions: List[TransactionModel]):
         self.product_id = product_id
-
         self._transactions = transactions
-
-        self._pm = 0
-        self._qtd = 0
-        self._total_price = 0
-        self._pm = 0
 
     @property
     def qtd(self) -> int:
-        last_transfer = None
+        qtd = 0
+        transfers = []
         for trx in self._transactions:
             if trx.type in ['empréstimo'] and trx.in_out == 'in':
-                last_transfer = trx
-            elif trx.type in ['transferência - liquidação', 'atualização']:
-                if last_transfer:
-                    last_transfer = None
-                elif trx.in_out == 'in':
-                    self._qtd += trx.qtd
+                if trx.qtd:
+                    transfers.append(trx)
                 else:
-                    self._qtd -= trx.qtd
-        return self._qtd
+                    try:
+                        transfers.pop(0)
+                    except IndexError:
+                        pass
+            elif trx.type in ['bonificação em ativos']:
+                qtd += trx.qtd
+            elif trx.type in ['leilão de fração']:
+                qtd -= trx.qtd
+            elif trx.type in ['transferência - liquidação', 'atualização']:
+                if transfers:
+                    transfers.pop(0)
+                elif trx.in_out == 'in':
+                    qtd += trx.qtd
+                else:
+                    qtd -= trx.qtd
+            elif trx.type in ['desdobro']:
+                qtd += trx.qtd
+
+        if self.type == 'Ações':
+            return int(qtd)
+
+        return qtd
 
     @property
     def pm(self) -> float:
         qtd = 0
         total_price = 0
+        pm = 0
 
         subscricoes = []
-        last_transfer = None
+        transfers = []
         for trx in self._transactions:
             if trx.type in ['direitos de subscrição - exercido']:
                 subscricoes.append(trx)
             elif trx.type in ['empréstimo'] and trx.in_out == 'in':
-                last_transfer = trx
+                if trx.qtd:
+                    transfers.append(trx)
+                else:
+                    try:
+                        transfers.pop(0)
+                    except IndexError:
+                        pass
+
+            elif trx.type in ['atualização'] and self.type == 'Ações':
+                qtd += trx.qtd
+                total_price += pm * trx.qtd
+
             elif trx.type in ['atualização']:
                 for sub in subscricoes:
                     if sub.qtd == trx.qtd and not sub.total_price:
                         qtd += trx.qtd
-                        total_price = qtd * self._pm
+                        total_price = qtd * pm
                     elif sub.qtd == trx.qtd and sub.total_price:
                         qtd += trx.qtd
                         total_price += sub.total_price
 
+            elif trx.type in ['bonificação em ativos', 'desdobro']:
+                qtd += trx.qtd
+                if trx.total_price:
+                    total_price += trx.total_price
+
+            elif trx.type in ['leilão de fração']:
+                qtd -= trx.qtd
+                total_price += trx.total_price
+
             elif trx.type == 'transferência - liquidação':
-                if last_transfer:
-                    last_transfer = None
+
+                if transfers:
+                    transfers.pop(0)
                 elif trx.in_out == 'in':
                     qtd += trx.qtd
                     if trx.total_price:
                         total_price += trx.total_price
-                        self._pm = total_price / qtd
                     else:
-                        total_price = qtd * self._pm
+                        total_price = qtd * pm
                 else:
-
                     qtd -= trx.qtd
-                    total_price -= (trx.qtd * self._pm)
+                    total_price -= (trx.qtd * pm)
 
-                    try:
-                        self._pm = total_price / qtd
-                    except ZeroDivisionError:
-                        self._pm = 0
-                        total_price = 0
+            try:
+                pm = total_price / qtd
+            except ZeroDivisionError:
+                pm = 0
+                total_price = 0
 
-            # if trx.type in ['transferência - liquidação', 'atualização', 'direitos de subscrição - exercido', 'empréstimo']:
-            # # if trx.type not in ['rendimento', 'transferência']:
+            # if trx.type in ['empréstimo']:
+            # # if trx.type in ['transferência - liquidação', 'atualização', 'direitos de subscrição - exercido', 'empréstimo']:
+            # # if trx.type not in ['rendimento', 'transferência', 'juros sobre capital próprio', 'dividendo',
+            # # #                     'juros sobre capital próprio - transferido', 'reembolso']:
+            #     print("####")
+            #     print(len(transfers))
             #     print(trx._raw_transaction)
+            #     print(trx.type)
             #     print(qtd)
             #     print(total_price)
             #     print(self._pm)
-            #     print(trx.type)
 
-        return round(self._pm, 2)
+        return round(pm, 2)
 
     @property
     def type(self) -> str:
         return ALLOWED_PRODUCTS[self.product_id]['type']
+
+    @property
+    def ideal(self) -> float:
+        return ALLOWED_PRODUCTS[self.product_id]['ideal']
